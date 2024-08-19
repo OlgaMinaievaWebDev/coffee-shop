b8b6454b512efca645469bc376a3552c42a89269
Olga <minaeva9@gmail.com>
2024-08-19 17:05:27 -0400

    adding second page

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,24 @@
 import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../components/Header";
-import Menu from "../components/Menu";
+import MenuItem from "../components/MenuItem";
+import menuItems from "../menuItems";
 
 function AppLayout() {
   const navigation = useNavigation();
+
+  const featuredMenuItems = menuItems.slice(0, 3); // For example, display the first 3 items
   return (
     <>
       <Header />
-      <main>
-        <Outlet />
+      <Outlet />
+      <main className="pt-4">
+        <div className="bg-lightBrown py-4">
+          <div className="container mx-auto flex flex-wrap justify-center">
+            {featuredMenuItems.map((item) => (
+              <MenuItem key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
       </main>
     </>
   );
