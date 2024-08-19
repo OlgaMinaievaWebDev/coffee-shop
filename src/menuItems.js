b10684413b88098222dc4806cedd0074f8bf5f99
Olga <minaeva9@gmail.com>
2024-08-19 16:23:36 -0400

    menuItems added

---
+++ src/menuItems.js
@@ -0,0 +1,94 @@
+const menuItems = [
+  {
+    name: "Colombian Dark Roast",
+    photo: "../public/coffee-bag.webp",
+    id: 1,
+    type: "Dark",
+    region: "Colombia",
+    description:
+      "A rich and bold coffee with deep chocolate notes and a hint of caramel sweetness.",
+    price: "$15.99",
+  },
+  {
+    name: "Ethiopian Medium Roast",
+    photo: "../public/coffee-bag.webp",
+    id: 2,
+    type: "Medium",
+    region: "Ethiopia",
+    description:
+      "Bright and fruity with notes of blueberry and citrus, balanced with a smooth finish.",
+    price: "$16.99",
+  },
+  {
+    name: "Brazilian Blonde Roast",
+    photo: "../public/coffee-bag.webp",
+    id: 2,
+    type: "Blonde",
+    region: "Brazil",
+    description:
+      "A light and smooth coffee with hints of nuts and mild citrus flavors.",
+    price: "$14.99",
+  },
+  {
+    name: "USA Dark Roast",
+    photo: "../public/coffee-bag.webp",
+    id: 3,
+    type: "Dark",
+    region: "USA",
+    description:
+      "Strong and bold with smoky flavors and a heavy body, perfect for a classic American coffee.",
+    price: "$13.99",
+  },
+  {
+    name: "Colombian Medium Roast",
+    photo: "../public/coffee-bag.webp",
+    id: 4,
+    type: "Medium",
+    region: "Colombia",
+    description:
+      "Balanced and smooth with a mix of fruity and nutty flavors, a great everyday coffee.",
+    price: "$15.49",
+  },
+  {
+    name: "Guatemalan Blonde Roast",
+    photo: "../public/coffee-bag.webp",
+    id: 5,
+    type: "Blonde",
+    region: "Guatemala",
+    description:
+      "Light and crisp with floral notes and a touch of sweetness, ideal for a refreshing cup.",
+    price: "$15.99",
+  },
+  {
+    name: "Sumatra Dark Roast",
+    photo: "../public/coffee-bag.webp",
+    id: 6,
+    type: "Dark",
+    region: "Sumatra",
+    description:
+      "Full-bodied and earthy with notes of dark chocolate and a rich, lingering finish.",
+    price: "$17.49",
+  },
+  {
+    name: "Kenyan Medium Roast",
+    photo: "../public/coffee-bag.webp",
+    id: 7,
+    type: "Medium",
+    region: "Kenya",
+    description:
+      "Bright acidity with a rich body, featuring notes of blackcurrant and a wine-like finish.",
+    price: "$16.49",
+  },
+  {
+    name: "Honduran Blonde Roast",
+    photo: "../public/coffee-bag.webp",
+    id: 8,
+    type: "Blonde",
+    region: "Honduras",
+    description:
+      "Sweet and mellow with hints of honey and mild fruit flavors, perfect for a light brew.",
+    price: "$14.99",
+  },
+];
+
+export default menuItems;
