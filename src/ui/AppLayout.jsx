1c412209bc064656d2ba55996bd501b649e45c82
Olga <minaeva9@gmail.com>
2024-08-19 17:13:43 -0400

    1

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,26 +1,16 @@
 import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../components/Header";
-import MenuItem from "../components/MenuItem";
-import menuItems from "../menuItems";
 
 function AppLayout() {
   const navigation = useNavigation();
 
-  const featuredMenuItems = menuItems.slice(0, 3); // For example, display the first 3 items
   return (
-    <>
+    <div>
       <Header />
-      <Outlet />
-      <main className="pt-4">
-        <div className="bg-lightBrown py-4">
-          <div className="container mx-auto flex flex-wrap justify-center">
-            {featuredMenuItems.map((item) => (
-              <MenuItem key={item.id} item={item} />
-            ))}
-          </div>
-        </div>
+      <main>
+        <Outlet />
       </main>
-    </>
+    </div>
   );
 }
 
