8673ea0db17513809c1b5cbb330da04e945eed74
Olga <minaeva9@gmail.com>
2024-08-20 16:51:11 -0400

    About style added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,10 @@
 function About() {
   return (
-    <div className="p-2 md:p-4 bg-darkerBrown text-white">
+    <div className="p-2 md:p-4 bg-darkerBrown text-white w-full">
       <h2 className="text-xl font-bold text-orange-dark text-center mb-4 uppercase">
         About Us
       </h2>
-      <div className="flex flex-col gap-3 md:flex-row md:gap-12 h-fit p-4">
+      <div className="flex flex-col gap-3 md:flex-row md:gap-12 h-full p-4">
         <p className="text-center md:text-left text-sm md:text-base">
           Founded in Toronto in 2016,{" "}
           <span className="text-orange">Roasters</span> began with a passion for
@@ -27,19 +27,19 @@ function About() {
       <h2 className="text-xl font-bold text-orange-dark text-center mb-4 uppercase">
         Our Coffee
       </h2>
-      <div className="flex flex-col gap-3 md:flex-row md:gap-12 h-fit p-4">
+      <div className="flex flex-col gap-3 md:flex-row md:gap-12 h-full p-4">
         <p className="text-center text-sm  md:text-left md:text-base">
-          At Roasters, our commitment to quality begins with the relationships
-          we've built with coffee farmers and producers around the world. These
-          partnerships are the foundation of our business, and we work closely
-          with our producers to ensure sustainable, long-term collaborations. We
-          source in-season coffee beans from exceptional growers to bring out
-          the best in every cup. Before a coffee makes it to our menu, it
-          undergoes rigorous cupping and tasting sessions to meet our high
-          standards. Our careful roasting process focuses on enhancing the
-          bean's natural flavors, reflecting its origin, varietal, and
-          processing style, so you can experience the true essence of each
-          coffee.
+          At <span className="text-orange">Roasters</span>, our commitment to
+          quality begins with the relationships we've built with coffee farmers
+          and producers around the world. These partnerships are the foundation
+          of our business, and we work closely with our producers to ensure
+          sustainable, long-term collaborations. We source in-season coffee
+          beans from exceptional growers to bring out the best in every cup.
+          Before a coffee makes it to our menu, it undergoes rigorous cupping
+          and tasting sessions to meet our high standards. Our careful roasting
+          process focuses on enhancing the bean's natural flavors, reflecting
+          its origin, varietal, and processing style, so you can experience the
+          true essence of each coffee.
         </p>
         <div className="flex items-center justify-center md:w-1/2">
           <img
@@ -52,15 +52,15 @@ function About() {
       <h2 className="text-xl font-bold text-orange-dark text-center mb-4 uppercase">
         Our Team
       </h2>
-      <div className="flex flex-col gap-3 md:flex-row md:gap-12 h-fit p-4">
+      <div className="flex flex-col gap-3 md:flex-row md:gap-12 h-full p-4">
         <p className="text-center md:text-left text-sm md:text-base">
-          At Roasters, we believe that everyone has a place here, regardless of
-          your background or goals. We're committed to creating an environment
-          where our team can grow, be supported, and explore their talents to
-          the fullest. We might be a small team, but we're driven by
-          collaboration, passion, and a relentless pursuit of excellence in
-          everything we do. Want to be part of our crew? Drop us a line at
-          info@roasters.com—we'd love to connect!
+          At <span className="text-orange">Roasters</span>, we believe that
+          everyone has a place here, regardless of your background or goals.
+          We're committed to creating an environment where our team can grow, be
+          supported, and explore their talents to the fullest. We might be a
+          small team, but we're driven by collaboration, passion, and a
+          relentless pursuit of excellence in everything we do. Want to be part
+          of our crew? Drop us a line at info@roasters.com—we'd love to connect!
         </p>
         <div className="flex items-center justify-center md:w-1/2">
           <img
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -6,7 +6,7 @@ function AppLayout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 flex items-center justify-center  bg-darkerBrown">
         <Outlet />
       </main>
       <Footer />
